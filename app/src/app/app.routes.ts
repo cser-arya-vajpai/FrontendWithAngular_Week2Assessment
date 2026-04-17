fee68f596cd2ab683cc6d53bb96c368e2ea2c419
cser-arya-vajpai <cser.arya.vajpai@gmail.com>
2026-04-18 00:21:15 +0530

    Delete app directory

--- app/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { MainLayout } from './layout/main-layout/main-layout';
-
-export const routes: Routes = [
-  {
-    path:'',
-    component:MainLayout
-  }
-];
